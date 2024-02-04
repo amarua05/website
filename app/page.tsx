e6f79338822ae8be759c04adef1c96245e36e252
Amar Emini <128543379+amarua05@users.noreply.github.com>
2024-02-04 19:15:52 +0100

    message to the world

--- app/page.tsx
+++ app/page.tsx
@@ -11,19 +11,19 @@ const Page: React.FC = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Card 1 */}
           <div className="bg-white p-6 rounded-md shadow-md">
-            <h2 className="text-xl font-semibold mb-4">Feature 1</h2>
+            <h2 className="text-xl font-semibold mb-4">It's</h2>
             <p className="text-gray-600">Description of feature 1 goes here.</p>
           </div>
 
           {/* Card 2 */}
           <div className="bg-white p-6 rounded-md shadow-md">
-            <h2 className="text-xl font-semibold mb-4">Feature 2</h2>
-            <p className="text-gray-600">Description of feature 2 goes here.</p>
+            <h2 className="text-xl font-semibold mb-4">Empty</h2>
+            <p className="text-gray-600">This website (sadly) is still under construction, check back in a while!</p>
           </div>
 
           {/* Card 3 */}
           <div className="bg-white p-6 rounded-md shadow-md">
-            <h2 className="text-xl font-semibold mb-4">Feature 3</h2>
+            <h2 className="text-xl font-semibold mb-4">At the moment :/</h2>
             <p className="text-gray-600">Description of feature 3 goes here.</p>
           </div>
         </div>
