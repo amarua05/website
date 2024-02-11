b7c8637f801913e53a7fc1dc73463c3dde83d8cf
Amar Emini <128543379+amarua05@users.noreply.github.com>
2024-02-11 19:49:53 +0100

    major update

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ const Page: React.FC = () => {
   return (
     <Layout>
     <Nav2 />
-    <div className="bg-white flex flex-col items-center justify-center h-screen">
-    <p className="font-extrabold text-5xl justify-center text-center">Under construction</p>
+    <div className="bg-gray-900 flex flex-col items-center justify-center h-screen z-0 overflow-hidden">
+    <p className="font-extrabold text-5xl justify-center text-center text-white">Under construction</p>
     <br />
     <img src="https://static.vecteezy.com/system/resources/previews/001/218/694/large_2x/under-construction-warning-sign-vector.jpg" className="" width="408" height="305" />
     <br />
-    <p className="font-oswald font-bold text-3xl text-center max-w-md">I&apos;m busy building something amazing! My website is under construction, but I can&apos;t wait to share it with you soon.</p>
+    <p className="text-white font-bold text-2xl text-center max-w-md">I&apos;m busy building something amazing! My website is under construction, but I can&apos;t wait to share it with you soon.</p>
     </div>
     
     </Layout>
--- components/Nav2.tsx
+++ components/Nav2.tsx
@@ -5,74 +5,78 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { BsList, BsX, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs'
 
-const styles = {
-  navLinks: 'cursor-pointer ml-10 border-b uppercase border-white hover:border-[#F6B519] text-xl'
-}
-
-
-
 function Nav2() {
   const[menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => setMenuOpen(!menuOpen);
   return (
   <header>
-    <nav className='flex w-full h-24 bg-gray-800 shadow-xl'>
+    <nav className='fixed flex w-full h-24 bg-gray-900 shadow-xl z-10'>
       {/**Desktop Menu */}
-      <div className='flex-1 flex items-center sm:justify-center justify-between h-full px-4 w-full'>
+      <div className='bg-gray-900 flex-1 flex items-center justify-between h-full px-4 w-full'>
       <Link href="https://amaremini.com">
       <Image className="cursor-pointer"
-           src="https://amaremini.com/favicon.ico" 
+           src="/../white-logo.png" 
            alt="Amar Emini Logo"
-           width={32}
-           height={32}
+           width={70}
+           height={70}
            />
-      </Link> 
-        
+      </Link>  
         <div className='hidden sm:flex text-white'>
           <ul className='hidden sm:flex'>
-            <li className={styles.navLinks}>
+            <li className='navLinksActive'>
+              <Link href="#">Home</Link>
+            </li>
+            <li className='navLinks'>
               <Link href="/aboutme">About Me</Link>
             </li>
-            <li className={styles.navLinks}> 
+            <li className='navLinks'> 
               <Link href="/projects">Projects</Link>
             </li>
-            <li className="flex items-center space-x-5 ml-10">
-              <Link href="/contactme">
-                <h3 className='cursor-pointer border border-[#F6B519] px-4 py-1 rounded-full bg-[#F6B519] text-black hover:bg-black hover:text-[#F6B519] ease-in-out duration-300'>Contact me</h3>
-              </Link>
+            <li className="navLinks">
+              <Link href="mailto:amaremini5@icloud.com">Contact me</Link>
             </li>
           </ul>
         </div>
-        {/* Mobile Menu */}
       <div onClick = {toggleMenu} className='sm:hidden cursor-pointer pl-24'>
         <BsList className='h-8 w-8 text-white'></BsList>
       </div>
       </div>
-      <div className={menuOpen ? 'fixed top-0 right-0 w-[75%] sm:hidden h-screen bg-gray-400 ease-in-out duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in-out duration-500'}>
-        <div className='flex w-full items-center justify-end'>
-          <div className='cursor-pointer'>
-            <BsX onClick = {toggleMenu} className='mt-4 h-12 w-12 text-black'></BsX>
-          </div>
+      {/* Mobile Menu */}
+      <div className={menuOpen ? 'fixed items-center top-0 right-0 w-[75%] sm:hidden h-full bg-black ease-in-out duration-500 shadow-' : 'fixed items-center top-0 right-[-100%] ease-in-out duration-500'}>
+        <div className='flex'>
+        <div className="flex flex-1"></div>
+        <div className='flex flex-1 justify-center'> {/** DIV 1 */}
+            <Image 
+            alt='Logo' 
+            src='/../white-logo.png' 
+            width={70} 
+            height={70} 
+            className='mt-[0.45rem]'>  
+            </Image>
         </div>
+        <div className='justify-end flex flex-1'> {/** DIV 2 */}
+            <BsX onClick = {toggleMenu} className='mt-4 mx-4 h-12 w-12 text-white cursor-pointer'></BsX>
+        </div>
+        </div>        
         {/* Mobile Menu Links */}
         <div className='flex flex-col py-4 items-center text-center'>
           <ul>
-            <li onClick = {() => setMenuOpen(false)} className='py-4 hover:underline hover:decoration-[#F6B519]'>
-            <Link href="/aboutme">About Me</Link>
+            <li onClick = {() => setMenuOpen(false)} className='navLinks'>
+            <Link className='text-white' href="/aboutme">About Me</Link>
             </li>
-            <li onClick = {() => setMenuOpen(false)}className='py-4 hover:underline hover:decoration-[#F6B519]'>
-            <Link href="/projects">Projects</Link>
+            <li onClick = {() => setMenuOpen(false)}className='navLinks'>
+            <Link className='text-white' href="/projects">Projects</Link>
             </li>
-            <li className='flex items-center py-4 text-[#F6B519]'>
-              <p className='cursor-pointer py-1 px-4 rounded-full bg-[#F6B519] text-black hover:text-[#F6B519] hover:bg-black ease-in-out duration-300'>Contact Me</p>
+            <li onClick = {() => setMenuOpen(false)}className='navLinks'>
+              <Link className='text-white' href="mailto:amaremini5@icloud.com">Contact Me</Link>
             </li>
           </ul>
         </div>
         {/**Social Media Links */}
         <div className='flex flex-row justify-around pt-10'>
-          <BsInstagram href='https://instagram.com/_aamaar1' className='cursor-pointer'></BsInstagram>
-          <BsLinkedin href='https://linkedin.com/in/amar-emini-b82309247' className='cursor-pointer'></BsLinkedin>
-          <BsGithub href='https://github.com/amarua05' className='cursor-pointer'></BsGithub>
+          <BsInstagram href='https://instagram.com/_aamaar1' className='cursor-pointer text-white'></BsInstagram>
+          <BsLinkedin href='https://linkedin.com/in/amar-emini-b82309247' className='cursor-pointer text-white'></BsLinkedin>
+          <BsGithub href='https://github.com/amarua05' className='cursor-pointer text-white'></BsGithub>
         </div>
       </div>
     </nav>
